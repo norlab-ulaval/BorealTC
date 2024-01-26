43f8d9fac72a10e83ca03e5088ae2a205e99a6f8
Damien LaRocque <phicoltan@gmail.com>
2024-01-25 23:05:34 -0500

    First commit in Python

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+AUTHOR INFORMATION AND SCRIPT OVERVIEW, V1.0, 12/2020
+Author:________________________________________Fabio Vulpi (Github: Ph0bi0)
+
+                                 PhD student at Polytechnic of Bari (Italy)
+                     Researcher at National Research Council of Italy (CNR)
+
+This is the main script to train and test deep terrain classification
+models:
+- Convolutional Neural Network (CNN)
+- Long Short-Term Memory recurrent neural network (LSTM)
+- Convolutional Long Short-Term Memory recurrent neural network (CLSTM)
+
+The script also uses a state of the art Support Vector Machine (SVM) as
+benchmark
+-------------------------------------------------------------------------
+"""
+
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+cwd = Path.cwd()
+data_dir = cwd / "datasets"
+
+
+
+
+
