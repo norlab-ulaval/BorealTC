9fb09829a64f0c9270bb6e2ab81e28cdebf6365e
Damien LaRocque <phicoltan@gmail.com>
2024-01-27 22:01:59 -0500

    Export mat files to CSV

---
+++ mat2csv.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import pandas as pd
+import scipy.io as scio
+
+mat_dir = Path("datasets")
+csv_dir = Path("data")
+
+cols = {
+    "imu": [
+        "gyrX",
+        "gyrY",
+        "gyrZ",
+        "accX",
+        "accY",
+        "accZ",
+    ],
+    "pro": [
+        "Lvel",
+        "Rvel",
+        "Lcur",
+        "Rcur",
+    ],
+}
+
+
+def mat_to_csv(fname: Path) -> Path:
+    terr = fname.parent.stem
+
+    datatype = fname.stem.split("_")[0]
+    data = scio.loadmat(fname, matlab_compatible=True)[datatype]
+    datacols = ["time", *cols[datatype]]
+
+    df = pd.DataFrame(data, columns=datacols)
+
+    csv_terr_dir = csv_dir / terr
+    csv_terr_dir.mkdir(exist_ok=True, parents=True)
+
+    export_path = (csv_terr_dir / fname.stem).with_suffix(".csv")
+    df.to_csv(export_path, index=False)
+
+    return export_path
+
+
+if __name__ == "__main__":
+    files = [*mat_dir.rglob("*.mat")]
+    for f in files:
+        mat_to_csv(f)
