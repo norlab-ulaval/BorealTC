a35b3c84332f0957ac41095699a3fb5df9fce78b
Damien LaRocque <phicoltan@gmail.com>
2024-01-29 11:59:33 -0500

    Add docstring and explicit that the method reads CSVs

--- README.md
+++ README.md
@@ -4,3 +4,27 @@ Terrain Classification via Deep Learning (MATLAB implementation)
 V1.0, 29/12/2020, Authors: F. Vulpi, G. Reina, A. Milella
 
 Details: Joint collaboration between Politecnico di Bari and CNR-STIIMA
+
+## Dataset organization
+
+```sh
+data
+├── CLASS1
+│   ├── imu_1.csv
+│   ├── imu_2.csv
+│   ├── ...
+│   ├── pro_1.csv
+│   ├── pro_2.csv
+│   └── ...
+└── CLASS2
+    ├── imu_1.csv
+    ├── imu_2.csv
+    ├── ...
+    ├── pro_1.csv
+    ├── pro_2.csv
+    └── ...
+```
+
+
+
+
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -8,10 +8,38 @@ import scipy.io as scio
 from sklearn.model_selection import StratifiedKFold
 
 
-def get_recordings(
+def get_csv_recordings(
     data_dir: Path,
     channels: dict[str, dict],
 ) -> dict[str, dict[str, list[pd.DataFrame]]]:
+    """Extract data from CSVs in data_dir and filter out the columns with `channels`
+
+    ```
+        data
+        ├── CLASS1
+        │   ├── imu_1.csv
+        │   ├── imu_2.csv
+        │   ├── ...
+        │   ├── pro_1.csv
+        │   ├── pro_2.csv
+        │   └── ...
+        └── CLASS2
+            ├── imu_1.csv
+            ├── imu_2.csv
+            ├── ...
+            ├── pro_1.csv
+            ├── pro_2.csv
+            └── ...
+    ```
+
+    Args:
+        data_dir (Path): Path to the dataset. The direct childs of data_dir are terrain classes folders
+        channels (dict[str, dict]): Description of the different channels
+
+    Returns:
+        dict[str, dict[str, list[pd.DataFrame]]]: Dictionary of dataframes
+            `{"CLASS1":{"imu":[imu1, imu2, ...], "pro":[pro1, pro2, ...]}, "CLASS2": {"imu":[imu1, imu2, ...], "pro":[pro1, pro2, ...]}}`
+    """
     # All terrains names
     terrains = [f.stem for f in data_dir.iterdir() if f.is_dir()]
 
