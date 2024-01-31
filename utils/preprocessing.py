778470c81772d027097d868763bb820d81c3850a
Damien LaRocque <phicoltan@gmail.com>
2024-01-31 00:35:25 -0500

    WIP on partition data

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -91,13 +91,14 @@ def partition_data_csv(
     # Highest sampling frequency
     hf_sensor = summary["sampling_freq"].idxmax()
     hf = summary["sampling_freq"].max()
+    # Other sensors are low frequency
     lf_sensors = tuple(sens for sens in data.keys() if sens != hf_sensor)
 
     # Time (s) / window * Sampling freq = samples / window
     wind_length = int(partition_duration * hf)
 
     # Create partition windows
-    partitions = {}
+    partitions = {hf_sensor: {}, **{s: {} for s in lf_sensors}}
 
     # Data from the high frequency sensor
     hf_data = data[hf_sensor]
@@ -109,99 +110,66 @@ def partition_data_csv(
         for exp_idx in exp_idxs:
             hf_exp = hf_terr[hf_terr.run_idx == exp_idx].copy().reset_index(drop=True)
 
+            # Get limits, avoid selecting incomplete partitions
             starts = np.arange(0, hf_exp.shape[0], wind_length)
+            starts = starts[(starts + wind_length) < hf_exp.shape[0]]
             limits = np.vstack([starts, starts + wind_length]).T
 
-            windows = [hf_exp.iloc[slice(*lim)] for lim in limits]
+            # Get multiple windows
+            windows = [
+                hf_exp.iloc[slice(*lim)].assign(win_idx=win_idx)
+                for win_idx, lim in enumerate(limits)
+            ]
+            tlimits = [np.array([w.time.min(), w.time.max()]) for w in windows]
+            partitions[hf_sensor].setdefault(terr, []).extend(windows)
 
-            pass
+            # Slice each lf sensor based on the time from the hf windows
+            for sens in lf_sensors:
+                lf_data = data[sens]
+                lf_terr = lf_data[lf_data.terrain == terr]
+                lf_exp = lf_terr[lf_terr.run_idx == exp_idx]
+                lf_exp = lf_exp.copy().reset_index(drop=True)
+                lf_time = lf_exp.time.to_numpy()[None, :]
 
-    #
+                indices = np.array(
+                    [np.abs(lf_time - tlim[:, None]).argmin(axis=1) for tlim in tlimits]
+                )
+                indices[:, 1] += 1
+                lf_win = [
+                    lf_exp.iloc[slice(*lim)].assign(win_idx=win_idx)
+                    for win_idx, lim in enumerate(indices)
+                ]
+                partitions[sens].setdefault(terr, []).extend(lf_win)
 
-    for sens, sensor_data in data.items():
-        # Primary sensor data (max sampling freq)
-        pass
+    columns = partitions[hf_sensor][terrains[0]][0].columns.values
+    unified = {
+        sens: np.vstack([sens_data[terr] for terr in terrains])
+        for sens, sens_data in partitions.items()
+    }
+    n_windows = unified[hf_sensor].shape[0]
 
+    # Split with K folds
     skf = StratifiedKFold(n_splits=n_splits, random_state=random_state, shuffle=True)
 
-    return (0, 0)
+    train_data, test_data = [], []
 
-    for i in range(NumTer):
-        k = 1
-        for j in range(len(REC["data"][TN[i]])):
-            strt = 0
-            stop = int(part_window * sf)
-
-            while stop <= len(REC["data"][TN[i]][j][c]):
-                t0 = REC["time"][TN[i]][j][c][strt]
-                t1 = REC["time"][TN[i]][j][c][stop]
-
-                PRT["time"].setdefault(TN[i], []).append(
-                    REC["time"][TN[i]][j][c][strt:stop]
-                )
-                PRT["data"].setdefault(TN[i], []).append(
-                    REC["data"][TN[i]][j][c][strt:stop, :]
-                )
-
-                for s in range(len(CN)):
-                    if s != c:
-                        e0 = np.argmin(np.abs(t0 - REC["time"][TN[i]][j][s]))
-                        e1 = np.argmin(np.abs(t1 - REC["time"][TN[i]][j][s]))
-
-                        PRT["time"].setdefault(TN[i], []).append(
-                            REC["time"][TN[i]][j][s][e0:e1]
-                        )
-                        PRT["data"].setdefault(TN[i], []).append(
-                            REC["data"][TN[i]][j][s][e0:e1, :]
-                        )
-
-                strt = stop + 1
-                stop = stop + int(part_window * sf)
-                k += 1
-
-    # Create a struct "UNF" containing all "PRT" data and time
-    # Create a categorical array "label" for partitioning
-    UNF = {
-        "data": np.empty((0, len(CN), None)),
-        "time": np.empty((0, len(CN), None)),
-        "labels": np.empty((0, 1)),
-    }
-
-    k = 0
-    for i in range(NumTer):
-        for j in range(len(PRT["data"][TN[i]])):
-            UNF["data"] = np.vstack(
-                (UNF["data"], np.expand_dims(PRT["data"][TN[i]][j], axis=0))
-            )
-            UNF["time"] = np.vstack(
-                (UNF["time"], np.expand_dims(PRT["time"][TN[i]][j], axis=0))
-            )
-            UNF["labels"] = np.vstack((UNF["labels"], i * np.ones((1, 1))))
-            k += 1
-
-    # Use StratifiedKFold to partition data
-    skf = StratifiedKFold(n_splits=KFOLD, shuffle=True, random_state=random_state)
-
-    # Create "Train" and "Test" structs
-    Train = {}
-    Test = {}
-
-    for i, (train_index, test_index) in enumerate(
-        skf.split(UNF["data"], UNF["labels"])
+    for fold_idx, (fold_train_idx, fold_test_idx) in enumerate(
+        skf.split(np.arange(n_windows), np.arange(n_windows))
     ):
-        Train["folder_" + str(i)] = {
-            "data": UNF["data"][train_index],
-            "time": UNF["time"][train_index],
-            "labl": UNF["labels"][train_index],
-        }
+        train_data.append(
+            {
+                sens: sens_data[fold_train_idx, :, :]
+                for sens, sens_data in unified.items()
+            }
+        )
+        test_data.append(
+            {
+                sens: sens_data[fold_test_idx, :, :]
+                for sens, sens_data in unified.items()
+            }
+        )
 
-        Test["folder_" + str(i)] = {
-            "data": UNF["data"][test_index],
-            "time": UNF["time"][test_index],
-            "labl": UNF["labels"][test_index],
-        }
-
-    return Train, Test
+    return train_data, test_data
 
 
 def augment_data(train_dat, test_dat, summary, w, AUG):
