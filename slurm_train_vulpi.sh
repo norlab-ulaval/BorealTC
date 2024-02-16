dd26dff5aa46c07f9ec81af82399276bf2106eb0
William Guimont-Martin <wil9876543210@hotmail.com>
2024-02-16 12:10:26 -0800

    husky and vulpi slurm jobs

---
+++ slurm_train_vulpi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=4-00:00
+#SBATCH --job-name=TerrainVulpi
+#SBATCH --output=%x-%j.out
+
+cd ~/Vulpi2021-terrain-deep-learning
+docker build -t terrain-gpu -f DockerfileGPU .
+docker run --gpus all -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES -e DATASET='vulpi' --rm --ipc host \
+  --mount type=bind,source=.,target=/code/ \
+  --mount type=bind,source=/dev/shm,target=/dev/shm \
+  terrain-gpu python3 main.py
