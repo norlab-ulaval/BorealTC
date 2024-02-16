e5560278bdf8459bf535d37c3dcb4bbe31944e64
William Guimont-Martin <wil9876543210@hotmail.com>
2024-02-16 16:56:05 -0500

    optuna tuning jobs

--- slurm_tune_lstm.sh
+++ slurm_tune_lstm.sh
@@ -7,7 +7,7 @@
 
 cd ~/Vulpi2021-terrain-deep-learning
 docker build -t terrain-gpu -f DockerfileGPU .
-docker run --gpus all -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES -e DATASET='husky' --rm --ipc host \
+docker run --gpus all -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES -e DATASET='vulpi' -e MODEL='LSTM' --rm --ipc host \
   --mount type=bind,source=.,target=/code/ \
   --mount type=bind,source=/dev/shm,target=/dev/shm \
-  terrain-gpu python3 main.py
+  terrain-gpu python3 optuna_tuning.py
