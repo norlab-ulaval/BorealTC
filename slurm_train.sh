460b09e0d18e8c335764ecf9a4b94f7adce4ce87
William Guimont-Martin <wil9876543210@hotmail.com>
2024-02-19 15:29:47 -0500

    trap signal in slurm scripts

--- slurm_train.sh
+++ slurm_train.sh
@@ -7,7 +7,16 @@
 
 cd ~/Vulpi2021-terrain-deep-learning
 docker build -t terrain-gpu -f DockerfileGPU .
-docker run --gpus all -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES --rm --ipc host \
-  --mount type=bind,source=.,target=/code/ \
-  --mount type=bind,source=/dev/shm,target=/dev/shm \
-  terrain-gpu python3 main.py
+container_id=$(
+  docker run --gpus all -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES --rm --ipc host \
+    --mount type=bind,source=.,target=/code/ \
+    --mount type=bind,source=/dev/shm,target=/dev/shm \
+    terrain-gpu python3 main.py
+)
+
+stop_container() {
+  docker container stop $container_id
+}
+
+trap stop_container EXIT
+docker wait $container_id
