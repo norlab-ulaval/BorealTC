48af5025cff397c7d5d1d09715fd962742bc6407
Damien LaRocque <phicoltan@gmail.com>
2024-02-22 19:04:36 -0500

    WIP on mixed partition

--- utils/mixed.py
+++ utils/mixed.py
@@ -98,47 +98,52 @@ def mx_partition_data(
     )
 
     # Highest sampling frequency
-    hf_sensor = summary["sampling_freq"].idxmax()
-    hf = summary["sampling_freq"].max()
+    lf_sensor = summary["sampling_freq"].idxmin()
+    lf = summary["sampling_freq"].min()
     # Other sensors are low frequency
-    lf_sensors = tuple(sens for sens in data.keys() if sens != hf_sensor)
+    hf_sensors = tuple(sens for sens in data.keys() if sens != lf_sensor)
 
     # Partitions
     partitions = {}
 
-    # Size of hf windows
-    hf_sz = partitioned[hf_sensor].shape[1]
+    # Size of lf windows
+    lf_sz = partitioned[lf_sensor].shape[1]
 
-    hf_data = data[hf_sensor]
-    hf_data["terr_idx"] = -1
-    hf_data["win_idx"] = -1
-    hf_cols = hf_data.columns.tolist()
-    hf_c = [*np.take(hf_cols, (-4, -2, -3, -1, 0)), *hf_cols[1:-4]]
+    lf_data = data[lf_sensor]
+    lf_data["terr_idx"] = -1
+    lf_data["win_idx"] = -1
+    lf_cols = lf_data.columns.tolist()
+    lf_c = [*np.take(lf_cols, (-4, -2, -3, -1, 0)), *lf_cols[1:-4]]
 
-    hf_arr = hf_data[hf_c].to_numpy()
-    hf_idxs = sliding_window_view(hf_data.index, hf_sz)
-    hf_wins = hf_arr[hf_idxs, :]
+    lf_arr = lf_data[lf_c].to_numpy()
+    lf_idxs = sliding_window_view(lf_data.index, lf_sz)
+    lf_wins = lf_arr[lf_idxs, :]
 
-    hf_tlims = hf_wins[:, [0, -1], ch_cols["time"]]
+    lf_tlim = lf_wins[:, 0, ch_cols["time"]]
 
-    partitions[hf_sensor] = hf_wins.copy()
+    partitions[lf_sensor] = lf_wins.copy()
 
-    for lf_sens in lf_sensors:
-        lf_sz = partitioned[lf_sens].shape[1]
+    for hf_sens in hf_sensors:
+        hf_sz = partitioned[hf_sens].shape[1]
 
-        lf_data = data[lf_sens]
-        lf_data["terr_idx"] = -1
-        lf_data["win_idx"] = -1
-        lf_cols = lf_data.columns.tolist()
-        lf_c = [*np.take(lf_cols, (-4, -2, -3, -1, 0)), *lf_cols[1:-4]]
+        hf_data = data[hf_sens]
+        hf_data["terr_idx"] = -1
+        hf_data["win_idx"] = -1
+        hf_cols = hf_data.columns.tolist()
+        hf_c = [*np.take(hf_cols, (-4, -2, -3, -1, 0)), *hf_cols[1:-4]]
 
-        lf_arr = lf_data[lf_c].to_numpy()
-        lf_time = lf_arr[:, ch_cols["time"]]
+        hf_arr = hf_data[hf_c].to_numpy()
+        hf_time = hf_arr[:, ch_cols["time"]]
 
-        lf_idxs = sliding_window_view(lf_data.index, lf_sz)
-        lf_wins = lf_arr[lf_idxs, :]
+        hf_tpad = np.repeat(hf_time[None, :], lf_tlim.shape, axis=0)
+        lf_tpad = np.repeat(lf_tlim[:, None], hf_time.shape, axis=1)
+        indices = lf_tpad[: lf_tlim // 2, :] - hf_tpad[: lf_tlim // 2, :]
+        print(indices.shape)
 
-        partitions[lf_sens] = lf_wins.copy()
+        hf_idxs = sliding_window_view(hf_data.index, hf_sz)
+        hf_wins = hf_arr[hf_idxs, :]
+
+        partitions[hf_sens] = hf_wins.copy()
 
         pass
 
