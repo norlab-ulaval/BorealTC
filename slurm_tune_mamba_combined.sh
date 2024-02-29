a0a902e6d479b74a2122d9e0b47f6f1fd36f937c
David-Alexandre Duclos <56205581+Asers387@users.noreply.github.com>
2024-02-29 08:59:27 -0500

    added slurm script for mamba combined

---
+++ slurm_tune_mamba_combined.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=4-00:00
+#SBATCH --job-name=TuneMambaCombined
+#SBATCH --output=%x-%j.out
+
+cd ~/Vulpi2021-terrain-deep-learning
+docker build -t terrain-gpu -f DockerfileGPU .
+container_id=$(
+  docker run --gpus all -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES -e DATASET='combined' -e MODEL='Mamba' --rm --ipc host \
+    --mount type=bind,source=.,target=/code/ \
+    --mount type=bind,source=/dev/shm,target=/dev/shm \
+    terrain-gpu python3 optuna_tuning.py
+)
+
+stop_container() {
+  docker container stop $container_id
+  docker logs $container_id
+}
+
+trap stop_container EXIT
+echo "Container ID: $container_id"
+docker wait $container_id
