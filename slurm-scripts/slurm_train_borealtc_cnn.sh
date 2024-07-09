b3ef748eac6e2ace97a841cb9a454b9e58521762
Damien LaRocque <phicoltan@gmail.com>
2024-07-09 16:43:49 -0400

    Add new slurm scripts

---
+++ slurm-scripts/slurm_train_borealtc_cnn.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --gres=gpu:a6000
+#SBATCH --cpus-per-task=8
+#SBATCH --time=4-00:00
+#SBATCH --job-name=TerrainCNNHusky
+#SBATCH --output=%x-%j.out
+
+cd ~/BorealTC
+buildah build --layers -t borealtc .
+container_id=$(
+  podman run --device nvidia.com/gpu=all \
+    -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+    -e DATASET='husky' --rm --ipc host \
+    --mount type=bind,source=.,target=/code/ \
+    --mount type=bind,source=/dev/shm,target=/dev/shm \
+    borealtc python3 cnn_train.py
+)
+
+stop_container() {
+  podman container stop $container_id
+  podman logs $container_id
+}
+
+trap stop_container EXIT
+echo "Container ID: $container_id"
+podman wait $container_id
